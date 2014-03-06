8bd23a4f75956837608f941f23ce1af56bf0e247
Prasenjit Mukherjee <pmukherj@clearpathrobotics.com>
2014-03-06 08:01:14 -0500

    fixed up cross track directional calculations, added multiple point tracking

--- calculate_errors.m
+++ calculate_errors.m
@@ -6,11 +6,11 @@ function [e_theta e_ct] = calculate_errors(p,theta, p1, p2)
     y2 = p2(2);
     
     %Goal Theta
-    theta_g = atan2((y1 - y2), (x1 - x2));
+    theta_g = atan2((y2 - y1), (x2 - x1));
     %Angular error, with care for rollover
     e_theta = atan2(sin(theta_g - theta), cos(theta_g - theta));
     
     % Distance from point to line segment
     % http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
-    e_ct = det([p2-p1;p-p1])/norm(p2-p1);
+    e_ct = det([p1-p2;p-p1])/norm(p1-p2);
 end
--- drive.m
+++ drive.m
@@ -1,16 +1,13 @@
 clc;clear all;close all
-tf = 10;
+tf = 40;
 dt = 0.1;
 x0 = 0;
 y0 = 0;
 theta0 = 0;
- 
-% xg = 3;
-% yg = 5;
+dist_thresh = 0.1;
 
-p1 = [3 5];
-p2 = [2 2];
-
+display_on = 1;
+p = [3 5; 3 -5;-3 5];
 t = 0:dt:tf;
 
 x = zeros(length(t));
@@ -22,51 +19,85 @@ x(1) = x0;
 y(1) = y0;
 theta(1) = theta0;
 
-k_p = 1;
+k_p = 0.5;
 h = figure(1);
 hold on;
 axis([-6 6 -6 6]);
+g = 1; %goal number
+
+p1 = [x0 y0];
+p2 = p(g,:);
+
+x1 = p1(1);
+y1 = p1(2);
+x2 = p2(1);
+y2 = p2(2);
+
+plot([x1 x2],[y1 y2], 'r');
+
 
 
 for i=2:length(t) 
     %% Plot Goal Line (and segment ends)
-    x1 = p1(1);
+    clf;
+    hold on; 
+    axis([-6 6 -6 6]);
+
+    diff = [x(i-1) y(i-1)] - p2;
+    dist = norm(diff);
+    
+    if (dist < dist_thresh)
+        if (g==numel(p))
+           break;
+        end
+        p1 = p(g,:);
+        p2 = p(g+1,:);
+        g = g + 1;
+       
+      
+    end
+
+     x1 = p1(1);
     y1 = p1(2);
     x2 = p2(1);
     y2 = p2(2);
-    m = (y2 - y1)/(x2 - x1);
-    b = y1 - m*x1;
-    x_line = 0:0.1:10;
-    y_line = m*x_line + b;
-    plot(x_line, y_line, 'r');
-    plot(x1, y1, 'r.');
-    plot(x2, y2, 'r.');
+
+        plot([x1 x2],[y1 y2], 'r');
     
     %% Plot Perpendicular crosstrack and angles error
+    
+    m = (y2 - y1)/(x2 - x1);
+    b = y1 - m*x1;
+    x_line = x1:0.1:x2;
+    y_line = m*x_line + b;
+    plot(x_line, y_line, 'r');
     m_p = -1/m;
     b_p = y(i-1) - m_p*(x(i-1));
-    
     %Find the intersection point
-    %m*x + b = m_p*x + b_p; 
-    %(m - m_p)*x = b_p - b;
-%     x_int = (b_p - b) / (m-m_p);
-%     y_int = m*x_int + b;
-%     plot([x(i-1) x_int], [y(i-1) y_int], 'r');
-%     plot([x(i-1) x(i-1) + 3*cos(theta(i-1))], [y(i-1) y(i-1) + 3*sin(theta(i-1))],'g');
-%     plot([x(i-1) x(i-1) + 3*cos(theta_g)], [y(i-1) y(i-1) + 3*sin(theta_g)],'b');
-    
+%     m*x + b = m_p*x + b_p; 
+%     (m - m_p)*x = b_p - b;
+    x_int = (b_p - b) / (m-m_p);
+    y_int = m*x_int + b;
+    plot([x(i-1) x_int], [y(i-1) y_int], 'r');
+
     %% Calculate errors
     [e_t d(i)] = calculate_errors([x(i-1) y(i-1)], theta(i-1), p1, p2);
     
     %% Calculate Controller
-    V = 1;
-    w = e_t + atan2(k_p * d(i), V);
-    
+    V = 0.5;
+    w = e_t + atan2(k_p * d(i),V);
+
     %% Simulate dynamics
     x(i) = x(i-1) + V*cos(theta(i-1))*dt;
     y(i) = y(i-1) + V*sin(theta(i-1))*dt;
     theta(i) = theta(i-1) + w*dt;
     drawcar(x(i),y(i),theta(i),0.5,h); 
+    pause(dt);
     
     
 end
+%%
+figure(2)
+plot(t, d);
+
+
